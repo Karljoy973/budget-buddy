7ddf52a06f1287b33bb4eac17f8c692420c80d3f
Karl-Maison <karljoyeux@gmail.com>
2023-08-02 16:59:41 +0200

    fixing database connexion

--- Server/index.js
+++ Server/index.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 const SV_PORT = process.env.SV_PORT;
 const DB_PORT = process.env.DB_PORT;
 
-connexion.connect((error) => {
-  if (error) console.warn(error);
-  else console.log(`Database Connection Setup on port ${DB_PORT}`);
-});
+// connexion.connect((error) => {
+//   if (error) console.warn(error);
+//   else console.log(`Database Connection Setup on port ${DB_PORT}`);
+// });
 
 app.listen(SV_PORT, () =>
   console.log(
