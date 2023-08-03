6d5f031119ba6b2e701118add384e827aad5e2a6
Karl-Maison <karljoyeux@gmail.com>
2023-08-03 13:13:25 +0200

    adding body parser middleware

--- Server/app/index.js
+++ Server/app/index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const logger = require("../log/index");
+var bodyParser = require('body-parser');
 const app = express();
+
+// ** midlleware1 section
 app.use(logger);
+
+
+
+//To parse URL encoded data
+app.use(bodyParser.urlencoded({ extended: false }))
+//To parse json data
+app.use(bodyParser.json())
+
+// ** end of middleware 1 section 
+
+// ** route handler
+
+// ** end of route handler
+
+// ** middleware 2 section
+
+// ** end of middleware 2 section
+
 
 module.exports = app;
